8314bfa3d729d20814a70fb1f2f03fe5c89f061d
Olionkey <Olionkeygaming@gmail.com>
2020-03-05 16:20:01 -0600

    Working edit, with edit

--- index.js
+++ index.js
@@ -16,36 +16,39 @@ client.on ("ready", () => {
 })
 
 client.on("message", message => {
-    if (message.channel.id == "684838856922628149"){
+    if (message.author.bot)
+        return;
+    if (message.channel.id == "685213596136767542"){
         let currentMessage = message.content;
-        if (!currentMessage.match(digitCheck) || currentMessage < currentCount || currentMessage > currentCount + 1 || message.author == lastUser || currentMessage != currentCount){
+        if (!currentMessage.match(digitCheck) || currentMessage < currentCount || currentMessage > currentCount + 1 || message.author == lastUser || currentMessage != currentCount || currentMessage.includes(".")){
             console.log( chalk.red(`Message delete ${currentMessage} , Message author ${message.author.username}`) );
-            statTracker.deleteUpdate(message.author.id, message.guild.id);
+            statTracker.deleteUpdate(message.author.id); // message.guild.id
             message.delete();
         }
         else{
             currentCount++;
-            lastUser = message.author;
+            lastUser = message.author.id;
             // Updates the current count for persitent storage.
             updateJsonFile("./config/config.json", (data) => {
                 data.currentNumber = currentCount;
                 data.lastUser = lastUser;
                 return data;
             });
-            statTracker.correctUpdate(message.author.id, message.guild.id);
+            statTracker.correctUpdate(message.author.id); // Message.guild.id
         }
     }
 })
 
 // Check to see if there is a message edited, if so move the counter down one and remove the message. 
 client.on("messageUpdate", message => {
-    if (message.channel.id == "684838856922628149"){
+    if (message.channel.id == "685213596136767542"){
         message.delete();
+        statTracker.editUpdate(message.author.id);
         currentCount--;
         updateJsonFile("./config/config.json", (data) => {
             data.currentNumber = currentCount;
             return data;
         })
-        message.channel.send("The current count has been reverted to, due to a message having an edit: " + currentCount);
+        message.channel.send(`A message was edited. The count has been reverted to **${currentCount}**`).then(msg => {msg.delete({timeout: 600000})});
     }
 })
