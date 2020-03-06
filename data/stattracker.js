8f8bc04df021cf3e743322ca35ac24405466f505
Olionkey <Olionkeygaming@gmail.com>
2020-03-06 12:44:02 -0600

    Started to add server speration, but running into issue where the findServer function does not work as intended

--- data/stattracker.js
+++ data/stattracker.js
@@ -1,45 +1,108 @@
 const fs = require("fs");
 const chalk = require("chalk");
 let statsFile = require("../data/stats.json"); // read the file into memory
+let serverIndex;
 
-exports.deleteUpdate = async (author) => {
-    let index = await findUser(author);
-    (index != -1) ? statsFile.users[index].delete = statsFile.users[index].delete + 1 : await createUser(author, 0);
-    updateUser();
+exports.deleteUpdate = async (author, server) => {
+    let index = await findUser(author, server);
+    let userObj = statsFile.servers[serverIndex];
+    userObj = userObj[server];
+    (index != -1) ? userObj.users[index].delete = userObj.users[index].delete + 1 : await createUser(author, 0, server);
+    updateFile();
 }
 
-exports.correctUpdate = async (author) => {
-    let index = await findUser(author);
-    (index != -1) ? statsFile.users[index].correct = statsFile.users[index].correct + 1 : await createUser(author, 1);
-    updateUser();
+exports.correctUpdate = async (author, server) => {
+    let index = await findUser(author, server);
+    let userObj = statsFile.servers[serverIndex];
+    userObj = userObj[server];
+    (index != -1) ? userObj.users[index].correct = userObj.users[index].correct + 1 : await createUser(author, 1, server);
+    updateFile();
 }
 
-exports.editUpdate = async (author) => {
-    let index = await findUser(author);
-    if (index != -1){
-        statsFile.users[index].correct = statsFile.users[index].delete + 1;
-        statsFile.users[index].correct = statsFile.users[index].edit + 1;
+exports.updateCountChannel = async (server, channel) => {
+    let tempObj = await findServer(server);
+    let serverObj = statsFile.servers[tempObj];
+    serverObj = serverObj[server];
+    serverObj.config.countChannel = channel;
+    updateFile();
+}
+
+exports.updateCountAndUser = async (server, count, user) => {
+    let tempObj = await findServer(server);
+    let serverObj = statsFile.servers[tempObj];
+    serverObj = serverObj[server];
+    serverObj.config.currentCount = count;
+    serverObj.config.lastUser = user;
+    updateFile();
+}
+
+exports.getPrefix = async (server) => {
+    console.log(`Server: ${server}`);
+    let tempObj = await findServer(server);
+    console.log(tempObj);
+    let serverObj = statsFile.servers[tempObj];
+    console.log(serverObj);
+    serverObj = serverObj[server];
+    console.log("serverobj " + serverObj);
+    return serverObj.config.prefix;
+}
+
+exports.createServer = async (server) => {
+    if (await findServer(server) != -1) return console.log(chalk.red ("Server has already been created"));
+    statsFile["servers"].push({ [server]: { "users": [], "config": { "prefix": "!", "currentCount": 0, "lastUser": "foo", "countChannel": 0 } } });
+    await updateFile();
+    console.log(chalk.green(`Server was created: ${server}`));
+}
+    
+exports.getAllChannels = async () => {
+    let channels = [];
+    for (let i = 0 ; i < statsFile.servers.length; i ++){
+        let tempObj = statsFile.servers[i];
+        tempObj = tempObj[Object.keys(statsFile.servers[i])];
+        channels.push(tempObj.config.countChannel);
     }
-    else
-        await createUser(author, 1)
-    updateUser();
+    return channels;
 }
 
-async function findUser(author) {
-    let users = statsFile.users;
-    for (let i = 0; i < users.length; i++)
-        if (users[i].author == author) return i;
+// Currently does not search correctly, it just pulls the first server index even if it is not equal.
+async function findServer(server) {
+    let servers = statsFile.servers;
+    console.log(servers);
+    for (let i = 0; i < servers.length; i++) {
+        if (servers[i] == server) console.log("hi");
+        if ((Object.keys(servers[i]).includes(server))) {
+            serverIndex = Object.keys(servers[i]).indexOf(server);
+            console.log(`Server index ${serverIndex}`);
+            console.log(`ServerIndexObjectKey ${Object.keys(servers[i]).indexOf(server)}`);
+            return serverIndex;
+        }
+    }
     return -1;
 }
 
-async function createUser(author, choice) {
-    if (choice == 0) statsFile['users'].push({ "author": author, "delete": 1, "correct": 0, "edit": 0 });
-    else if (choice == 1) statsFile['users'].push({ "author": author, "delete": 0, "correct": 0, "edit":0 });
-    else if (choice == 2) statsFile['users'].push({ "author": author, "delete": 0, "correct": 1, "edit": 1 });
-    console.log(chalk.green(`User was created: ${author}`))
+async function findUser(author, server) {
+    let serverObj = await findServer(server);
+    let tempObj = statsFile.servers[serverObj];
+    tempObj = tempObj[server]
+    let userObj = tempObj.users;
+    for (let i = 0 ; i < userObj.length; i ++)
+        if(userObj[i].author == author) return i;
+    return -1;
 }
 
-async function updateUser() {
-    let data = JSON.stringify(statsFile);
-    fs.writeFileSync("./data/stats.json", data);
+async function createUser(author, choice, server) {
+    let file = statsFile.servers[serverIndex];
+    file = file[server];
+    if (choice == 0) file['users'].push({ "author": author, "delete": 1, "correct": 0 });
+    else if (choice == 1) file['users'].push({ "author": author, "delete": 0, "correct": 1 });
+    else file['users'].push({ "author": author, "delete": 0, "correct": 1 });
+    console.log(chalk.green(`User was created: ${author}`));
+    updateFile();
 }
+
+async function updateFile() {
+    let data = JSON.stringify(statsFile, null, 4);
+    fs.writeFileSync("./data/stats.json", data);
+    console.log("file updated");
+}
+
--- index.js
+++ index.js
@@ -6,49 +6,68 @@ const updateJsonFile    = require("update-json-file");
 const statTracker       = require("./data/stattracker.js");
 const chalk             = require("chalk");
 
+
 let currentCount        = config.currentNumber;
 let lastUser            = config.lastUser;
 let digitCheck          = /\d+/g;
+let channels            = [];
 
 client.login(auth.token);
-client.on ("ready", () => {
+client.on ("ready", async () => {
     console.log("I am on, hello world!!");
+    channels = await statTracker.getAllChannels();
 })
 
-client.on("message", message => {
+// Create a new server when the bot joins a new server.
+client.on('guildCreate', async guild => {
+    await statTracker.createServer(guild.id);
+    console.log(chalk.green("Server has been created, with guild id: " + guild.id));
+});
+
+client.on("message", async message => {
+    if (message.author.bot) return;
+    let serverPrefix = await statTracker.getPrefix(message.guild.id);
+    const args = message.content.slice(serverPrefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    switch (command){
+        case "bindcount":
+            if (message.member.hasPermission("MANAGE_CHANNELS")){
+                await statTracker.updateCountChannel(message.guild.id, message.channel.id);
+                message.channel.send("Channel has been bound").then(msg => { msg.delete({ timeout: 120000})});
+            } else 
+                message.channel.send("Sorry but you are missing the MANAGE_CHANNELS permission").then(msg => { msg.delete({ timeout: 120000 }) });
+        break;
+
+    }
+})
+
+client.on("message", async message => {
     if (message.author.bot)
         return;
-    if (message.channel.id == "685213596136767542"){
+    if (channels.includes(message.channel.id)){
         let currentMessage = message.content;
         if (!currentMessage.match(digitCheck) || currentMessage < currentCount || currentMessage > currentCount + 1 || message.author == lastUser || currentMessage != currentCount || currentMessage.includes(".")){
             console.log( chalk.red(`Message delete ${currentMessage} , Message author ${message.author.username}`) );
-            statTracker.deleteUpdate(message.author.id); // message.guild.id
-            message.delete();
+            await statTracker.deleteUpdate(message.author.id, message.guild.id); // message.guild.id
+            //message.delete();
         }
         else{
             currentCount++;
             lastUser = message.author.id;
             // Updates the current count for persitent storage.
-            updateJsonFile("./config/config.json", (data) => {
-                data.currentNumber = currentCount;
-                data.lastUser = lastUser;
-                return data;
-            });
-            statTracker.correctUpdate(message.author.id); // Message.guild.id
+            await statTracker.updateCountAndUser(message.guild.id, currentCount, lastUser);
+            await statTracker.correctUpdate(message.author.id, message.guild.id); // Message.guild.id
         }
     }
 })
 
 // Check to see if there is a message edited, if so move the counter down one and remove the message. 
-client.on("messageUpdate", message => {
-    if (message.channel.id == "685213596136767542"){
+client.on("messageUpdate", async message => {
+    if (channels.includes(message.channel.id)){
         message.delete();
-        statTracker.editUpdate(message.author.id);
+        await statTracker.editUpdate(message.author.id);
         currentCount--;
-        updateJsonFile("./config/config.json", (data) => {
-            data.currentNumber = currentCount;
-            return data;
-        })
+        await statTracker.updateCountAndUser(message.guild.id, currentCount, lastUser);
         message.channel.send(`A message was edited. The count has been reverted to **${currentCount}**`).then(msg => {msg.delete({timeout: 600000})});
     }
 })
